e70f161d5ca19bfab4e43c18437c9c9d9a0d9339
SMee <smeemarket@gmail.com>
2022-03-23 01:30:10 +0630

    first commit

---
+++ resources/js/Pages/TestOne.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="bg-info">
+        <Link href="test-two" class="btn btn-danger">to Two</Link>
+        <h1 class="text-primary">Test One Component</h1>
+    </div>
+</template>
+<script>
+import { Link } from '@inertiajs/inertia-vue3'
+export default {
+    components:{
+        Link
+    }
+}
+</script>
---
+++ resources/js/Pages/TestTwo.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="bg-warning">
+        <Link :href="route('testOne')" class="btn btn-danger">to One</Link>
+        <h1 class="text-secondary">Test Two Component</h1>
+    </div>
+</template>
+<script>
+import { Link } from '@inertiajs/inertia-vue3'
+export default {
+    components:{
+        Link
+    }
+}
+</script>
